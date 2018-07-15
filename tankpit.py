32d565b31dde3626832716319ce610e0e88b297f
Zephireis <Travisclarkakatc@hotmail.com>
2018-07-15 11:53:53 -0400

    Update tankpit.py

--- tankpit.py
+++ tankpit.py
@@ -19,15 +19,15 @@ client = Bot (command_prefix=BOT_PREFIX)
 async def eight_ball(context):
 
     possible_responses = [
-    'okay'
-    'sure'
-    'no'
-    'huh'
-    'idc'
-    'thats lame'
-    'yeah'
-    'dont do that'
-    'no that sucks'
+    'okay',
+    'sure',
+    'no',
+    'huh',
+    'idc',
+    'thats lame',
+    'yeah',
+    'dont do that',
+    'no that sucks',
 
     ]
     await client.say(random.choice(possible_responses) + "," + context.message.author.mention)
