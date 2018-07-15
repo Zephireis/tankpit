72d7fc263a9e55eabb07b01990010d6756e31525
Zephireis <Travisclarkakatc@hotmail.com>
2018-07-15 07:52:08 -0400

    Update tankpit.py

--- tankpit.py
+++ tankpit.py
@@ -10,31 +10,38 @@ TOKEN = "MjcxMzcwODc3NzAxMDYyNjY3.DiwnTQ.I7WcYZDw2oyRKeUOo-3PsAJQrVk"
 
 client = Bot (command_prefix=BOT_PREFIX)
 
-@client.command(name='8ball',
-                description="Answers = a yes/no question.",
-                brief="Answers from the beyond.",
+@client.command(name='Alzan',
+                description="Answers = a yes/no question with typical alzan responses.",
+                brief="Answers from Alzan the great.",
                 pass_context=True)
 
 
 async def eight_ball(context):
 
     possible_responses = [
-    'That is a resounding no',
-    'It is not looking likely',
-    'Too Hard to Tell',
-    'it is quite possible',
-    'Probably',
+    'okay',
+    'sure',
+    'no',
+    'huh',
+    'idc'
+    'thats lame'
+    'yeah'
 
     ]
     await client.say(random.choice(possible_responses) + "," + context.message.author.mention)
 
 @client.event
 async def on_ready():
-    await client.change_presence(game=Game(name="DESERT"))
+    await client.change_presence(game=Game(name="With Kassondra"))
     print("logged in as" + client.user.name)
 
 
-
+@client.command()
+async def bitcoin():
+    url = 'https://api.coindesk.com/v1/bpi/currentprice/BTC.json'
+    response = requests.get(url)
+    value = response.json() ['bpi']['USD']['rate']
+    await client.say("Bitcoin price is: $" + value)
 
 
 
