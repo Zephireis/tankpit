733744158c99e730f392201d11a32c8d37c29b99
Zephireis <Travisclarkakatc@hotmail.com>
2018-07-15 12:51:17 -0400

    Update tankpit.py

--- tankpit.py
+++ tankpit.py
@@ -6,7 +6,7 @@ from discord import Game
 from discord.ext.commands import Bot
 
 BOT_PREFIX = "*"
-TOKEN = "s"
+TOKEN = "Mjc3NTc1MTQwOTc0MjY0MzMz.DizE0g.rkkhFclCMNBYW6whhgd0_EKIZHY"
 
 client = Bot (command_prefix=BOT_PREFIX)
 
@@ -31,21 +31,25 @@ async def eight_ball(context):
 
     ]
     await client.say(random.choice(possible_responses) + "," + context.message.author.mention)
-    
-    
 
-  
+
+@client.event
+async def on_message():
+  if message.content == "hi":
+   if message.author.id == ('107678929925484544'):
+    choicesa = ('get away', 'dont greet me','dont talk to me fagget')
+    await bibi.say(random.choice(choicesa))
+  elif message.author.id == ('80971950054187008'):
+    choicesl = ('lauren...', 'hi. lauren.')
+    await bibi.say(random.choice(choicesl))
+
+
+
+
+
+
 
 @client.event
 async def on_ready():
     await client.change_presence(game=Game(name="With Kassondra"))
     print("logged in as" + client.user.name)
-
-
-
-
-
-
-
-
-client.run(TOKEN)
