4100ee2523cca678150ad7632b4c5339758455ea
Zephireis <Travisclarkakatc@hotmail.com>
2018-07-15 22:15:07 -0400

    Update tankpit.py

--- tankpit.py
+++ tankpit.py
@@ -6,7 +6,7 @@ from discord import Game
 from discord.ext.commands import Bot
 
 BOT_PREFIX = "*"
-TOKEN = "Mjc3NTc1MTQwOTc0MjY0MzMz.Di2O3A.sDz1C5jZkHUnYMo7tV5fF2fwF8k"
+TOKEN = "Mjc3NTc1MTQwOTc0MjY0MzMz.Di2RBQ.7ykAa_A8V5Y8eVEGxsApig_F-SA"
 
 client = Bot (command_prefix=BOT_PREFIX)
 
@@ -20,7 +20,7 @@ async def eight_ball(context):
 
     possible_responses = [
     'okay',
-    'totally',
+    'sure',
     'no',
     'huh',
     'idc',
@@ -28,24 +28,25 @@ async def eight_ball(context):
     'yeah',
     'dont do that',
     'no that sucks',
+    'totally'
 
     ]
-    
     await client.say(random.choice(possible_responses) + " " + context.message.author.mention)
+    
+    
 
-
-
-
-
-
-
-
-
+  
 
 @client.event
 async def on_ready():
     await client.change_presence(game=Game(name="With Kassondra"))
     print("logged in as" + client.user.name)
-  
+
+
+
+
+
+
+
+
 client.run(TOKEN)
-  
