0fe2f55c14bd1e16674d203342e48657255a28a2
Zephireis <Travisclarkakatc@hotmail.com>
2020-04-15 18:45:01 -0400

    Update tankpit.py

--- tankpit.py
+++ tankpit.py
@@ -1014,8 +1014,8 @@ async def update_stats():
                 await client.send_message(channel, embed=embed)
             else:
                 pass
-            await asyncio.sleep(10)
+            await asyncio.sleep(60)
 
 
-
+client.loop.create_task(update_stats())
 client.run(TOKEN)
